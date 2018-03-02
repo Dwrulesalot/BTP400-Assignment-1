203f17c181bddf1a3f1947620132779405f5ceda
Daniel Wierzbicki <dwwierzbicki@myseneca.ca>
2018-03-01 22:28:11 -0500

    added methods

--- GIC.java
+++ GIC.java
@@ -18,7 +18,48 @@ public class GIC extends Account
   {
     super(); //call to Account 0-arg constructor
     periodOfInvestment = 1;
-    intrestRate = 0.0125f;//Daniel: not sure if f is needed
+    interestRate = 0.0125f;//Daniel: not sure if f is needed
+  }
+  
+  //Deposit
+  public boolean deposit(BigDecimal amount){// No transactions can be made on a GIC account
+		return false;
+  }
+
+  //Withdrawal
+  public boolean withdraw(BigDecimal amount){// No transactions can be made on a GIC account
+		return false;
+    }
+	
+	public BigDecimal getBalanceAtMaturity(){
+		BigDecimal futureBalance;
+		futureBalance = pow((getAccountBalance()*(1+interestRate)), periodOfInvestment);//pow returns a double and also expects a double, will this work? do I need to cast?
+		
+		return futureBalance;
+		
+	}
+	
+	//Equals operator
+	public equals(Object acc){
+    boolean result = false;
+
+    if ( acc instanceof Account ){
+		Account acc2 = (Account) acc;
+
+		if ( (acc2.fullName.equals(fullName)) &&
+			(acc2.accountNum.equals(accountNum)) &&
+			(acc2.accountBalance.equals(accountBalance)) &&
+			(acc2.periodOfInvestment==serviceCharge) &&
+			(acc2.interestRate==transactions)
+
+			result = true;
+    }
+    return result;
+  }
+  
+  //toString
+  public toString(){
+    super.toString();
   }
 
 }
